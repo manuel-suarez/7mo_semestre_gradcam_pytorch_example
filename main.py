671dbbdb4f4e44422653d3c9ce9b9e9d60e97e37
Arturo Suárez <manuel.suarez@cimat.mx>
2024-08-05 11:31:47 -0600

    Main example

---
+++ main.py
@@ -0,0 +1,80 @@
+import warnings
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
+from torchvision.models.segmentation import deeplabv3_resnet50
+import torch
+import torch.functional as F
+import numpy as np
+import requests
+import torchvision
+from PIL import Image
+from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
+
+image_url = "9606553_ccc7518589_z.jpg"
+image = np.array(Image.open(image_url))
+rgb_img = np.float32(image) / 255
+input_tensor = preprocess_image(rgb_img,
+                                mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+model = deeplabv3_resnet50(pretrained=True, progress=False)
+model = model.eval()
+
+if torch.cuda.is_available():
+    model = model.cuda()
+    input_tensor = input_tensor.cuda()
+
+output = model(input_tensor)
+print(type(output), output.keys())
+
+class SegmentationModelOutputWrapper(torch.nn.Module):
+    def __init__(self, model):
+        super(SegmentationModelOutputWrapper,self).__init__()
+        self.model = model
+
+    def forward(self, x):
+        return self.model(x)["out"]
+
+model = SegmentationModelOutputWrapper(model)
+print(model)
+output = model(input_tensor)
+#print(type(output), output)
+
+normalized_masks = torch.nn.functional.softmax(output, dim=1).cpu()
+sem_classes = [
+        '__background__', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+        'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+        'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+        ]
+sem_class_to_idx = {cls: idx for (idx, cls) in enumerate(sem_classes)}
+
+car_category = sem_class_to_idx["car"]
+car_mask = normalized_masks[0, :, :, :].argmax(axis=0).detach().cpu().numpy()
+car_mask_uint8 = 255 * np.uint8(car_mask == car_category)
+car_mask_float = np.float32(car_mask == car_category)
+
+both_images = np.hstack((image, np.repeat(car_mask_uint8[:, :, None], 3, axis=-1)))
+img = Image.fromarray(both_images)
+img.save('result01.png')
+
+from pytorch_grad_cam import GradCAM
+
+class SemanticSegmentationTarget:
+    def __init__(self, category, mask):
+        self.category = category
+        self.mask = torch.from_numpy(mask)
+        if torch.cuda.is_available():
+            self.mask = self.mask.cuda()
+
+    def __call__(self, model_output):
+        return (model_output[self.category, :, :] * self.mask).sum()
+
+target_layers = [model.model.backbone.layer3]
+targets = [SemanticSegmentationTarget(car_category, car_mask_float)]
+with GradCAM(model=model,
+             target_layers=target_layers) as cam:
+    grayscale_cam = cam(input_tensor=input_tensor,
+                        targets=targets)[0, :]
+    cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+
+img = Image.fromarray(cam_image)
+img.save('result02.png')
